73a34c27627c34ce49b45c12b07b0d4fb9c81661
Junior <web2ajax@gmail.com>
2018-02-17 02:55:07 -0200

    Update function.js

--- function.js
+++ function.js
@@ -1,58 +1,60 @@
-// Pull in Firebase and GCloud deps
-var firebase = require('firebase');
-var gcloud = require('gcloud')({
-  projectId: process.env.GCP_PROJECT,
+import * as functions from 'firebase-functions';
+
+// Firebase
+import * as admin from 'firebase-admin';
+admin.initializeApp(functions.config().firebase);
+
+
+// Cloud Vision
+import * as vision from '@google-cloud/vision';
+const visionClient =  new vision.ImageAnnotatorClient();
+
+const bucketName = 'tesla-369.appspot.com';
+
+
+
+export const tesla369OCRImagem = functions.storage
+    
+    .bucket(bucketName)
+    .object()
+    .onChange( async event => {
+
+     //se o arquivo não existe retorna falso
+      if (event.data.resourceState == 'not_exists') return false;
+
+            const object = event.data;
+            const filePath = object.name;   
+
+            const imageUri = `gs://${bucketName}/${filePath}`;
+
+            const docId = filePath.split('.jpg')[0];
+
+            const docRef  = admin.firestore().collection('photos').doc(docId);
+            //se entrar entrar png não faz nada
+            if (filePath.endsWith('.png')) return false;
+            //se entrar pdf não faz nada
+            if (!filePath.endsWith('.pdf')) return false;
+
+            // Text Extraction
+            const textRequest = await visionClient.documentTextDetection(imageUri)
+            const fullText = textRequest[0].textAnnotations[0]
+            const text =  fullText ? fullText.description : null
+
+            // Web Detection
+            const webRequest = await visionClient.webDetection(imageUri)
+            const web = webRequest[0].webDetection
+
+            // Faces    
+            const facesRequest = await visionClient.faceDetection(imageUri)
+            const faces = facesRequest[0].faceAnnotations
+
+            // Landmarks
+            const landmarksRequest = await visionClient.landmarkDetection(imageUri)
+            const landmarks = landmarksRequest[0].landmarkAnnotations
+            
+            // Save to Firestore
+            const data = { text, web, faces, landmarks }
+            return docRef.set(data)
+                
+
 });
-
-// Initialize Firebase App with service account
-// TODO: get values from https://firebase.google.com/docs/server/setup#initialize_the_sdk
-firebase.initializeApp({
-  serviceAccount: {
-    "project_id": "<your-project-id>",
-    "private_key": "<your-private-key>",
-    "client_email": "<your-service-account-email>"
-  },
-  databaseURL: "<your-database-url>"
-});
-
-// Get GCS, Cloud Vision API
-var gcs = gcloud.storage(),
-    vision = gcloud.vision();
-
-function visiondetect(context, data) {
-  if (data !== undefined) {
-    // Create Firebase Storage public URL
-    var urlString = "https://firebasestorage.googleapis.com/v0/b/" + data.bucket + "/o/" + data.name.replace(/\//, '%2F') + "?alt=media&token=" + data.metadata.firebaseStorageDownloadTokens;
-
-    // Create GCS File from the data
-    var file = gcs.bucket(data.bucket).file(data.name);
-
-    // Use GCS File in the Cloud Vision API
-    vision.detectLabels(file, {
-      verbose: true
-    }, function(err, labels, apiResponse) {
-      if (err) {
-        context.failure("Vision detection failed")
-      } else {
-        var ref = firebase.database().ref("images");
-        ref.push({
-          "url": urlString,
-          "name": data.name,
-          "labels": labels
-        }, function(error) {
-          if (error) {
-            context.failure("Firebase write failed")
-          } else {
-            context.success(labels, apiResponse);
-          }
-        });
-      }
-    });
-  } else {
-    context.failure('No data payload defined!');
-  }
-}
-
-module.exports = {
-  visiondetect: visiondetect
-};
